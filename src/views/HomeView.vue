546e837afadd43653dc625a4e027c90df25b8863
Luciano Reis <lucianoreis.dev@gmail.com>
2022-09-22 20:28:18 -0400

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted, ref } from "vue"
+const marcas = ref([])
+const getBrands = () => {
+  fetch('https://parallelum.com.br/fipe/api/v2/cars/brands')
+    .then(response => response.json())
+    .then(data => {
+      marcas.value = data
+    })
+}
+onMounted(() => {
+  getBrands()
+})
+const texto = ref("CONCIPE")
+</script>
+<template>
+<ul>
+  <li v-for="marca in marcas" :key="marca.code">
+    {{ marca.name }}
+  </li>
+</ul>
+</template>
+
+<style>
+</style>

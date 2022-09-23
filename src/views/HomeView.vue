9800f8e964e7c6c7f29d7f382c9e7f44ee2d1d01
Luciano Reis <lucianoreis.dev@gmail.com>
2022-09-22 20:42:05 -0400

    Select tipos de veiculos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,25 @@
 <script setup>
 import { onMounted, ref } from "vue"
 const marcas = ref([])
-const getBrands = () => {
-  fetch('https://parallelum.com.br/fipe/api/v2/cars/brands')
+const getBrands = (vehiclesType) => {
+  fetch(`https://parallelum.com.br/fipe/api/v2/${vehiclesType}/brands`)
     .then(response => response.json())
     .then(data => {
       marcas.value = data
     })
 }
 onMounted(() => {
-  getBrands()
 })
 const texto = ref("CONCIPE")
 </script>
 <template>
+<div>
+  <select>
+    <option value="cars" @click="getBrands('cars')">Carros</option>
+    <option value="motorcycles" @click="getBrands('motorcycles')">Motos</option>
+    <option value="trucks" @click="getBrands('trucks')" >Caminhões</option>
+  </select>
+</div>
 <ul>
   <li v-for="marca in marcas" :key="marca.code">
     {{ marca.name }}

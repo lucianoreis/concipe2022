682dfb1a976ff6d1d74cd5f7b0d76a20da2bfaf2
Luciano Reis <lucianoreis.dev@gmail.com>
2022-09-22 21:08:37 -0400

    Requisição de modelos

--- src/views/ModelosView.vue
+++ src/views/ModelosView.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { computed, onMounted, ref, watch } from "vue"
+import { computed, onMounted, ref } from "vue"
+import { useRoute } from 'vue-router'
+const route = useRoute()
 const modelos = ref([])
 const getModels = (vehiclesType, brandId) => {
-  fetch(`https://parallelum.com.br/fipe/api/v2/${vehiclesType}/brands/${brandId}`)
+  fetch(`https://parallelum.com.br/fipe/api/v2/${vehiclesType}/brands/${brandId}/models`)
     .then(response => response.json())
     .then(data => {
       modelos.value = data
     })
 }
 onMounted(() => {
+  getModels(route.params.vehiclesType, route.params.brandId)
 })
 </script>
 

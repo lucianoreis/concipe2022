09913d4f872462e21397573b32db471db3e6207c
Luciano Reis <lucianoreis.dev@gmail.com>
2022-09-22 21:35:31 -0400

    Filtrar modelos

--- src/views/ModelosView.vue
+++ src/views/ModelosView.vue
@@ -3,6 +3,12 @@ import { computed, onMounted, ref } from "vue"
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const modelos = ref([])
+const busca = ref('')
+const filteredModels = computed(() => {
+  return modelos.value.filter(e => {
+    return e.name.toLowerCase().indexOf(busca.value.toLowerCase()) > -1
+  })
+})
 const getModels = (vehiclesType, brandId) => {
   fetch(`https://parallelum.com.br/fipe/api/v2/${vehiclesType}/brands/${brandId}/models`)
     .then(response => response.json())
@@ -17,8 +23,13 @@ onMounted(() => {
 
 <template>
   <div>
+    <input
+      type="text"
+      v-model="busca"
+      placeholder="Busque um modelo"
+    />
     <li
-      v-for="modelo in modelos"
+      v-for="modelo in filteredModels"
       :key="modelo.code"
     >
     {{ modelo.name }}
